c4810d7dae0b41ea57404445e303edb59c5e04af
atlani_m <mylan.atlani@epitech.eu>
2017-06-16 10:35:59 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/gametitle.js
+++ js/gametitle.js
@@ -1,28 +1,29 @@
 /**
  * Created by atlani_m on 15/06/2017.
  */
-var gameTitle = function(game){}
+var gameTitle = function(game) {
+    var logo;
+}
 
 gameTitle.prototype = {
-    preload: function() {
+    preload: function () {
 
-    this.game.load.image('phaser', 'assets/pole-emploi.jpg');
-    this.game.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/v2/filters/Pixelate.js');
-
+        this.game.load.image('phaser', 'assets/pole-emploi.jpg');
+        this.game.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/v2/filters/Pixelate.js');
     },
     create: function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaser');
+        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaser');
 
-        logo.anchor.set(0.5);
-
+        this.logo.anchor.set(0.5);
         var filter = this.game.add.filter('Pixelate', 800, 600);
+        this.logo.filters = [filter];
 
-        logo.filters = [filter];
+        this.game.add.tween(filter).to({sizeX: 100, sizeY: 100}, 5000, "Quad.easeInOut", true, 0, -1, true);
 
-        this.game.add.tween(filter).to( { sizeX: 100, sizeY: 100 }, 5000, "Quad.easeInOut", true, 0, -1, true);
-        this.playTheGame();
+        this.time.events.add(2500, this.change, this);
     },
-    playTheGame: function () {
-        this.game.state.start("Indoor");
+    change: function() {
+        this.game.state.start('Indoor');
     }
 }
+
